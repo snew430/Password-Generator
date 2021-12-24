5d4957389c0af50b108eb424e37829fbc038b402
snew430 <seanpnew@hotmail.com>
2021-12-23 22:13:56 -0500

    added ask function and object

--- script.js
+++ script.js
@@ -1,5 +1,63 @@
 // Assignment code here
 
+const uppercase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+const lowercase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+const number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const secChar = ["!", "@", "#", "$", "%", "&", "*", "?"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,8 +68,85 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
 
+//
+//
+
+function initAsk() {
+  // initial ask
+
+  //   made passLength into an integer "nine" changed to "9"
+  var passLength = parseInt(
+    prompt(
+      "How long would you like your password to be? Please enter a number between 8 and 128."
+    )
+  );
+
+  if (isNaN(passLength) === true) {
+    alert("Please enter a number");
+    return;
+  }
+
+  // make sure the password is the proper length
+  if (passLength < 8 || passLength > 128) {
+    // if not the proper length, ask again
+    alert(
+      "Your password must be between 8 and 128 characters.  Please choose again..."
+    );
+    return;
+  }
+
+  var upCasePass = confirm("Would you like to include upper case letters?");
+  var lowCasePass = confirm("Would you like to include lower case letters?");
+  var numPass = confirm("Would you like to include numbers?");
+  var specPass = confirm("Would you like to include special characters?");
+
+  if (!upCasePass && !lowCasePass && !numPass && !specPass) {
+    alert("You must choose at least one type of character");
+    return;
+  }
+
+  var userChoices = {
+    passLength: passLength,
+    upCasePass: upCasePass,
+    lowCasePass: lowCasePass,
+    numPass: numPass,
+    specPass: specPass,
+  };
+  console.log(userChoices);
+  return userChoices;
+}
+
+function randomValue(arr) {
+  var indexNumber = Math.floor(Math.random() * arr.length);
+  var indexContent = arr[indexNumber];
+  return indexContent;
+}
+
+for (i = 1; i < passLength; i++) {
+    
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", initAsk);
+
+//
+//
+//
+// GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+// WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
